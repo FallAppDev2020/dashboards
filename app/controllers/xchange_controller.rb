b8aeb1d5eec083537976939e88ef7ab896f401f0
OLauterbach <olauterb@chicagobooth.edu>
2020-10-28 05:34:46 +0000

    Finished rates

--- app/controllers/xchange_controller.rb
+++ app/controllers/xchange_controller.rb
@@ -1,21 +1,34 @@
 class XchangeController < ApplicationController
 
+#    <%@array_of_symbols.each do |a_symbol|%>
+#    <% end %>
+
   def show_coins
-    #@array_of_symbols = 
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symhash = @parsed_data.fetch("symbols")
+    @symkeys = @symhash.keys
     render ({:template => "forex_templates/choose_source.html.erb"})
   end 
   
 #  Parameters: {"convert_from"=>"USD", "convert_to"=>"USD"}
   def select_coin
-    #@array_of_symbols = 
-    @src = params.fetch("convert_from").capitalize
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symhash = @parsed_data.fetch("symbols")
+    @symkeys = @symhash.keys
+    @src = params.fetch("convert_from").upcase
     render ({:template => "forex_templates/choose_target.html.erb"})
   end 
   
 
   def convert_coin
-    @fsrc = params.fetch("convert_from").capitalize
-    @src2 = params.fetch("convert_to").capitalize
+    @src = params.fetch("convert_from").upcase
+    @src2 = params.fetch("convert_to").upcase
+    @raw_data = open("https://api.exchangerate.host/convert?" + "from=" + @src + "&to=" + @src2).read
+    @parsed_data = JSON.parse(@raw_data)
+    p @inf = @parsed_data.fetch("info")
+    p @rat = @inf.fetch("rate")
     render ({:template => "forex_templates/convert_coin.html.erb"})
 
   end 
